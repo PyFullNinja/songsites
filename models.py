7db71afc583af2ff677051b3389e38729723dfa4
PYproger <go4839it2561@gmail.com>
2025-12-25 13:30:15 +0200

    добавил емайл

--- models.py
+++ models.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(100), nullable=True)
+    
 
     def __repr__(self):
         return f'<User {self.username}>'
